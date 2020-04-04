bca28d695e184ec17c53d877c0a8c013c3b5f49a
Soundbirds <61962365+Soundbirds@users.noreply.github.com>
2020-04-04 16:58:38 -0700

    Update Push.bat

--- Push.bat
+++ Push.bat
@@ -2,4 +2,4 @@ cd  C:\Users\John\NY.Times.COVID19.googleVis.github.io
 git add COVID_counties.htm
 git add COVID_states.htm
 git commit --amend --no-edit  
-git push -u -v origin master
+git push -u -v --forced origin master
