2e08ba8cffeef5e1055e8e04f44be56bb9271490
Soundbirds <61962365+Soundbirds@users.noreply.github.com>
2020-04-04 16:48:15 -0700

    Create mainLoop.R

---
+++ mainLoop.R
@@ -0,0 +1,24 @@
+# dir.create("C:/Users/John/", showWarnings = FALSE, recursive = TRUE)
+
+while(TRUE) {
+    
+    setwd("C:/Users/John/")
+    
+    shell("echo git config --global user.name 'John Wallace' > run.bat")
+    shell("echo git config --global user.email 'soundbirds@gmail.com'  >> run.bat")
+    shell("echo git clone https://github.com/Soundbirds/NY.Times.COVID19.googleVis.github.io.git  >> run.bat")
+    shell("echo exit >> run.bat")
+    shell("start run.bat")
+    Sys.sleep(3)
+    shell("del run.bat")
+    Sys.sleep(3)
+    
+    setwd("C:/Users/John/NY.Times.COVID19.googleVis.github.io")
+    source('updateGvisFigs.R') 
+    
+    shell("start Push.bat")
+           
+    Sys.sleep(6*3600)
+}   
+ 
+
