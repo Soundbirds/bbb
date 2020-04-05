a72c67ee6e0c5885d9a4a729bccaf72a3d46f9ff
Soundbirds <61962365+Soundbirds@users.noreply.github.com>
2020-04-04 17:55:23 -0700

    Update updateGvisFigs.R

--- updateGvisFigs.R
+++ updateGvisFigs.R
@@ -34,7 +34,7 @@
       usCounties$newCases <- c(0, diff(usCounties$cases))
       usCounties$newDeaths <- c(0, diff(usCounties$deaths))
       usCounties$countyState <- paste(usCounties$county, usCounties$state, sep="_")
-      cat("\nCounties' data latest date:", format(usCounties$date[nrow(usCounties)]), "\n")
+      cat("\nCounties' data latest date:", format(usCounties$date[nrow(usCounties)]), "\n\n")
  
       # State <- c('Washington', 'New York', 'California')[1]
       # plot(gvisMotionChart(usCounties[usCounties$state %in% State & usCounties$date > "2020-02-24", ], idvar = 'county', timevar = 'date', 
