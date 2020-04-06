8f9e8e4f9dfa7b91ffdb1f719e9c7202b927862a
Soundbirds <61962365+Soundbirds@users.noreply.github.com>
2020-04-05 18:47:44 -0700

    Update updateGvisFigs.R

--- updateGvisFigs.R
+++ updateGvisFigs.R
@@ -13,7 +13,7 @@
       deathsNewByState <- NULL
       caseDoubleByState <- NULL
       for ( i in unique(usStates$state))  {
-        # print(i)
+        print(i)
         stateData <- usStates[usStates$state %in% i, ]
         casesNewByState <- rbind(casesNewByState, data.frame(date = stateData$date, state = stateData$state, newCases = c(0, diff(stateData$cases))))
         deathsNewByState <- rbind(deathsNewByState, data.frame(date = stateData$date, state = stateData$state, newDeaths = c(0, diff(stateData$deaths))))
@@ -36,17 +36,32 @@
 
       usCounties <- read.csv(textConnection(getURL("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")))
       usCounties$date <- as.Date(usCounties$date)
-      usCounties$newCases <- c(0, diff(usCounties$cases))
-      usCounties$newDeaths <- c(0, diff(usCounties$deaths))
       usCounties$countyState <- paste(usCounties$county, usCounties$state, sep="_")
       cat("\nCounties' data latest date:", format(usCounties$date[nrow(usCounties)]), "\n\n")
+      
+      casesNewByCounty <- NULL
+      deathsNewByCounty <- NULL
+      caseDoubleByCounty <- NULL
+      for ( i in unique(usCounties$countyState))  {
+        print(i)
+        countyData <- usCounties[usCounties$countyState %in% i, ]
+        casesNewByCounty <- rbind(casesNewByCounty, data.frame(date = countyData$date, state = countyData$state, newCases = c(0, diff(countyData$cases))))
+        deathsNewByCounty <- rbind(deathsNewByCounty, data.frame(date = countyData$date, state = countyData$state, newDeaths = c(0, diff(countyData$deaths))))
+        caseDoubleByCounty <- rbind(caseDoubleByCounty, data.frame(date = countyData$date, state = countyData$state, casesCntyDoublingRate = c(0, 1/diff(log(countyData$cases, 2)))))
+      }
+      
+      usCounties <-  match.f(usCounties, casesNewByCounty, c('date', 'state'), c('date', 'state'), 'newCases')
+      usCounties <-  match.f(usCounties, deathsNewByCounty, c('date', 'state'), c('date', 'state'), 'newDeaths')
+      usCounties <-  match.f(usCounties, caseDoubleByCounty, c('date', 'state'), c('date', 'state'), 'casesCntyDoublingRate')
+      usCounties$casesCntyDoublingRate[!is.finite(usCounties$casesCntyDoublingRate) | usCounties$casesCntyDoublingRate > 40] <- NA # Extreme cases removed
+     
  
       # State <- c('Washington', 'New York', 'California')[1]
       # plot(gvisMotionChart(usCounties[usCounties$state %in% State & usCounties$date > "2020-02-24", ], idvar = 'county', timevar = 'date', 
       #            xvar = 'deaths',  yvar = 'cases', sizevar = 'newCases', colorvar = 'newDeaths', options=list(width = 1365, height = 768)))
        
-      # Counties inside 3 states 
-      States <- c('Washington', 'New York', 'California')
+      # Counties inside 4 states 
+      States <- c('Washington', 'New York', 'California', 'Louisiana')
       Mc <- googleVis::gvisMotionChart(usCounties[usCounties$state %in% States & usCounties$date > "2020-02-24", ], idvar = 'countyState', timevar = 'date', 
            xvar = 'deaths',  yvar = 'cases', sizevar = 'newCases', colorvar = 'newDeaths', options=list(width = 1365, height = 768))
       # googleVis:::plot.gvis(Mc)      
