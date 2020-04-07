348b3dd124c49153025061d076dd941fcd4bfdd1
Soundbirds <61962365+Soundbirds@users.noreply.github.com>
2020-04-07 00:54:49 -0700

    add index.htm

--- Push.bat
+++ Push.bat
@@ -1,6 +1,7 @@
 cd  C:\Users\John\NY.Times.COVID19.googleVis.github.io
 git add COVID_counties.htm
 git add COVID_states.htm
+git add index.htm
 git commit --amend --no-edit  
 git push -u -v --force origin master
 exit
