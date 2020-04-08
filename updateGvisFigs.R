e56ddc44fed18d0eb632bc846c08c569aaf5cb38
Soundbirds <61962365+Soundbirds@users.noreply.github.com>
2020-04-07 17:03:48 -0700

    Updated Geo Chart to latest - still commented out
    Would need too much work to fix the all the fips issues.

--- updateGvisFigs.R
+++ updateGvisFigs.R
@@ -55,7 +55,9 @@
         casesNewByCounty <- rbind(casesNewByCounty, data.frame(date = countyData$date, countyState = countyData$countyState, newCases = c(0, diff(countyData$cases))))
         deathsNewByCounty <- rbind(deathsNewByCounty, data.frame(date = countyData$date, countyState = countyData$countyState, newDeaths = c(0, diff(countyData$deaths))))
         caseDoubleByCounty <- rbind(caseDoubleByCounty, data.frame(date = countyData$date, countyState = countyData$countyState, casesCntyDoublingRate = c(0, 1/diff(log(countyData$cases, 2)))))
-      }
+        # usCountiesLast <- rbind(usCountiesLast, data.frame(countyState = countyData$countyState, fips = countyData$fips, 
+        #     latestCases = countyData$cases[countyData$date %in% max(countyData$date)], latestDeaths = countyData$deaths[countyData$date %in% max(countyData$date)]))
+     }
       
       usCounties <-  match.f(usCounties, casesNewByCounty, c('date', 'countyState'), c('date', 'countyState'), 'newCases')
       usCounties <-  match.f(usCounties, deathsNewByCounty, c('date', 'countyState'), c('date', 'countyState'), 'newDeaths')
@@ -78,18 +80,16 @@
       # Some of the NY Times data like New York city don't have a fips, and some important fips are missing from housingData::geoCounty - so can't do this correctly!!!!!!!
       # # gvisGeoChart on latest County (fips) data (NY Times data is cummulative so just need that latest date)
       
-      #  usCountiesLast <- usCounties[usCounties$date %in% max(usCounties$date), ]
-      #  usCountiesLast <- match.f(usCountiesLast, housingData::geoCounty, 'fips', 'fips', c('lon', 'lat')) # lat/lon centroid of the counties from housingData
-      #  usCountiesLast <- renum(na.omit(usCountiesLast))
-      #  
-      #  usCountiesLast$LatLong <- paste0(usCountiesLast$lat,":", usCountiesLast$lon) 
-      #  
-      #  maxD <- max(usCountiesLast$deaths)
-      #  cMax <- gvisGeoChart(usCountiesLast, "LatLong", sizevar = 'cases', colorvar = 'deaths', 
-      #            options=list(region='US', width = 1365, height = 768, 
-      #                                     displayMode='markers',
-      #  				   colorAxis=paste0("{values:[1, ", 0.25*maxD, ", ", 0.50*maxD, ", ", 0.75*maxD, ", ", maxD, "], ",
-      #                                     "colors:['#FFFF00', '#EEEE00', '#EE2C2C', '#CD2626', '#8B1A1A']}")))
+      # usCountiesLast <- usCountiesLast[!duplicated(usCountiesLast$countyState), ]
+      # usCountiesLast <- match.f(usCountiesLast, housingData::geoCounty, 'fips', 'fips', c('lon', 'lat'), round = FALSE) # lat/lon centroid of the counties from housingData package
+      # usCountiesLast <- renum(na.omit(usCountiesLast))
+      # usCountiesLast$LatLong <- paste0(usCountiesLast$lat,":", usCountiesLast$lon) 
+      # maxD <- max(usCountiesLast$latestDeaths)
+      # cMax <- gvisGeoChart(usCountiesLast, "LatLong", sizevar = 'latestCases', colorvar = 'latestDeaths', 
+      #           options=list(region='US', width = 1365, height = 768, 
+      #                                    displayMode='markers',
+      #    colorAxis=paste0("{values:[1, ", 0.25*maxD, ", ", 0.50*maxD, ", ", 0.75*maxD, ", ", maxD, "], ",
+      #                                    "colors:['#FFFF00', '#EEEE00', '#EE2C2C', '#CD2626', '#8B1A1A']}")))
       #  # googleVis:::plot.gvis(cMax)
       #  googleVis:::print.gvis(cMax, file = 'index.htm')  # Put this Geo Chart on the main index.htm
       #  
